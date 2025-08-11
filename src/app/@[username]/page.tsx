2537dfd1750be0b22dec52cff3c65a568a013671
Brian Phobos <40128659+brianphobos@users.noreply.github.com>
2025-08-10 23:44:18 -0700

    Update page.tsx
    Server Wrapper Only Change

--- src/app/@[username]/page.tsx
+++ src/app/@[username]/page.tsx
@@ -1,28 +1,8 @@
-import { getAccount } from '@/lib/api';
+// src/app/[handle]/page.tsx
 import dynamic from 'next/dynamic';
-import ChannelHeader from '@/components/ChannelHeader';
+const ChannelClient = dynamic(() => import('./ChannelClient'), { ssr: false });
 
-const ChannelGrid = dynamic(() => import('@/components/ChannelGrid'), { ssr: false });
-
-export default async function ChannelPage({ params }: { params: { handle: string } }) {
-  // params.handle will be 'brianphobos' or '@brianphobos'
+export default function ChannelPage({ params }: { params: { handle: string } }) {
   const user = params.handle.replace(/^@/, '');
-  const account = await getAccount(user);
-
-  if (!account) {
-    return (
-      <div className="p-8 text-center text-red-400">
-        Channel @{user} not found.
-      </div>
-    );
-  }
-
-  return (
-    <div className="max-w-[1200px] mx-auto">
-      <ChannelHeader account={account} />
-      <div className="mt-6">
-        <ChannelGrid username={user} />
-      </div>
-    </div>
-  );
+  return <ChannelClient user={user} />;
 }
